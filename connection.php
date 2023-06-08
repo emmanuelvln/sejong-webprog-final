77bee8230a32d74e3f5c97fb94ace4de85cacd40
Emmanuel Vouillon <emmanuel.vouillon@gmail.com>
2023-06-08 14:57:07 +0900

    adds tables creation if not exists

--- connection.php
+++ connection.php
@@ -11,4 +11,31 @@
 	mysqli_query($con, $query) or die(mysqli_error($con));
 
 	mysqli_select_db($con, $dbname) or die(mysqli_error($con));
+
+	$query = 'CREATE TABLE IF NOT EXISTS users (
+		id BIGINT(20) NOT NULL AUTO_INCREMENT,
+		user_id BIGINT(20) NOT NULL,
+		user_name VARCHAR(100) NOT NULL,
+		password VARCHAR(100) NOT NULL,
+		email TEXT NOT NULL,
+		date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		PRIMARY KEY (id)
+		)
+		ENGINE=MyISAM';
+
+	mysqli_query($con, $query) or die(mysqli_error($con));
+
+	$query = 'CREATE TABLE IF NOT EXISTS content (
+		id BIGINT(20) NOT NULL AUTO_INCREMENT,
+		url TEXT NOT NULL,
+		title TEXT NOT NULL,
+		user VARCHAR(100) NOT NULL,
+		user_id BIGINT(20) NOT NULL,
+		location TEXT NOT NULL,
+		date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		PRIMARY KEY (id)
+		)
+		ENGINE=MyISAM';
+
+	mysqli_query($con, $query) or die(mysqli_error($con));
 ?>
